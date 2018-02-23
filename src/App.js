8ae095bbf1951319913d9106f00cc0e0a00460c9
SeaFalcon <seafalcon323@gmail.com>
2018-02-23 18:42:30 +0900

    Second Commit

--- src/App.js
+++ src/App.js
@@ -42,6 +42,28 @@ class App extends Component {
     }
   }
 
+  handleToggle = id => {
+    const {todos} = this.state;
+
+    // 파라미터로 받은 id 를 가지고 몇번째 아이템인지 찾습니다.
+    const index = todos.findIndex(todo => todo.id === id);
+    const selected = todos[index]; // 선택한 객체
+
+    const nextTodos = [...todos]; // 배열을 복사
+    console.log(index, selected, nextTodos);
+
+    // 기존의 값들을 복사하고, checked 값을 덮어쓰기
+    nextTodos[index] = {
+      ...selected,
+      checked: !selected.checked
+    };
+
+    this.setState({
+      todos: nextTodos
+    });
+
+  }
+
   render() {
     const { input, todos } = this.state;
     // const input = this.state.input;
@@ -49,7 +71,8 @@ class App extends Component {
     const {
       handleChange,
       handleCreate,
-      handleKeyPress
+      handleKeyPress,
+      handleToggle
     } = this;
     //const handleChange = this.handleChange;
     //const handleCreate = this.handleCreate;
@@ -63,7 +86,7 @@ class App extends Component {
           onChange={handleChange}
           onCreate={handleCreate}
         />}>
-        <TodoItemList todos={todos}/>
+        <TodoItemList todos={todos} onToggle={handleToggle}/>
       </TodoListTemplate>
     );
   }
